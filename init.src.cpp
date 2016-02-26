7bb247192fa872816f6d0da09452bc52a049d135
MoonfireSeco <haaloplaysminecraft@gmail.com>
2016-02-26 15:25:40 +0200

    Update init.src.cpp

--- init.src.cpp
+++ init.src.cpp
@@ -7,7 +7,7 @@ using namespace std;
 ifstream in("flori2.in");
 ofstream out("flori2.out");
 
-/*METHOD1 struct nr
+struct nr
 {
     int ctr=0; //in cati vectori se afla
     int v[151]; //in care vectori se afla
@@ -19,21 +19,21 @@ bool AlreadyExists(int v[],int lim,int x)
         if(v[i]==x) return true;
     }
     return false;
-}*/
+}
 
-//METHOD1 nr jojer[101];
+nr jojer[101];
 
-//METHOD1 int content[151][151];
-int v[101][151]; //NUMARUL_FLORII | NRDEVECTORI+VECTORII
+int content[151][151];
+
 int main()
 {
     freopen("in.txt","r",stdin);
     int load,quant;
     int cobai;
+    int winrar,maxi;
     cin>>load>>quant;
-    
-    for()
-    /*METHOD1 for(register int i=0;i<=load-1;i++)
+
+    for(register int i=0;i<=load-1;i++)
     {
         for(register int k=0;k<=quant-1;k++)
         {
@@ -45,9 +45,10 @@ int main()
             }
             content[0][k]=cobai;
         }
-    }*/
-    
-    /*AFISARE for(register int i=0;i<=10;i++)
+    }
+
+    //AFISAJ
+    for(register int i=0;i<=10;i++)
     {
         cout<<"pentru "<<i<<" avem "<<jojer[i].ctr<<" aparitii, in vectorii:\n";
         for(register int k=0;k<=jojer[i].ctr-1;k++)
@@ -55,15 +56,5 @@ int main()
             cout<<jojer[i].v[k]<<'\n';
         }
         cout<<'\n';
-    }*/
-    
-    /*METHOD1 for(register int i=0;i<=quant-1;i++) content[0][i]=0;
-
-    int cont=0;
-    int fini=0;
-
-    for(register int i=0;i<=100 && fini<load;i++)
-    {
-        
-    }*/
+    }
 }
